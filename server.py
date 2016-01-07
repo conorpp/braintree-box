1df8de89de4000c2fe1be3142665fa9c5c14b926
Conor Patrick <conorpp@vt.edu>
2016-01-07 00:13:47 -0500

    improvements

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
+import smtplib
+from email.mime.text import MIMEText
+
 import braintree,json
 from flask import Flask,request
+from flask.ext.cors import CORS
 
 conf = json.loads(open('config.json','r').read())
 
-
+origin = None
 
 if conf['environment'] == 'sandbox':
     braintree.Configuration.configure(braintree.Environment.Sandbox,
@@ -13,39 +17,61 @@ if conf['environment'] == 'sandbox':
                                       public_key=conf['sandbox-pubkey'],
                                       private_key=conf['sandbox-privkey'],
                                       )
+    origin = conf['sandbox-origin']
+
 else:
     braintree.Configuration.configure(braintree.Environment.Sandbox,
                                       merchant_id=conf['merchant-id'],
                                       public_key=conf['pubkey'],
                                       private_key=conf['privkey'],
                                       )
+    origin = conf['origin']
 
-app = Flask(__name__)
 
+app = Flask(__name__, static_url_path='')
 
+CORS(app, resources = {r'*':{'origins':origin}})
+
+index = open('index.html').read()
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
-
+def root():
+    return index
 
 
 @app.route('/client_token', methods=['GET'])
 def client_token():
-    return request.args.get('callback')+'("' +braintree.ClientToken.generate() + '")';
-
+    return braintree.ClientToken.generate();
 
 
 @app.route('/checkout', methods=['POST'])
 def create_purchase():
-    nonce = request.form['payment_method_nonce']
+    trans = request.json
+    nonce = trans.get('nonce','asdfghjkl')
+    name = trans.get('name', None)
+    if name is None:
+        return json.dumps({'status':'fail', 'errors':['No name supplied']})
+    name = name[0:50]
     result = braintree.Transaction.sale({
-            'amount':'10.00',
-            'payment_method_nonce': nonce
+        'amount':'5.00',
+        'payment_method_nonce': nonce
         })
-    print 'got result' , result
-
-
+    if result.is_success:
+        return json.dumps({'status':'success', 'name':name})
+    else:
+        return json.dumps({'status':'fail', 'errors': [x.message for x in result.errors.deep_errors]})
 
 if __name__ == '__main__':
+    """
+    msg = MIMEText('\ntest email\n')
+    msg['Subject'] = 'thanks for your purchase'
+    msg['From'] = 'noreply@conorpp.com'
+    msg['To'] = 'conorpp@vt.edu'
+
+    s = smtplib.SMTP('localhost')
+    s.sendmail('noreply@conorpp.com', 'conorpp@vt.edu', msg.as_string())
+
+    print 'send mail?'
+    """
+
     app.run(host='127.0.0.1')
